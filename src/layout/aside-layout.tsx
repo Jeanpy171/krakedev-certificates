7185a5427e574fa5e7810a30595c0ad04ce3cf5d
Jeanpy171 <jeanpyfuentes67@gmail.com>
2025-01-22 09:38:18 -0500

    fix create certificates

--- src/layout/aside-layout.tsx
+++ src/layout/aside-layout.tsx
@@ -8,10 +8,10 @@ export const AsideLayout = (props: {
     <div
       className={`flex flex-col md:flex-row h-full w-full gap-3 ${props.className}`}
     >
-      <section className="flex h-2/5 w-full md:w-2/5 md:h-full">
+      <section className="flex w-full md:w-2/5 h-full">
         {props.children[0]}
       </section>
-      <aside className="flex h-3/5 w-ful md:w-3/5 md:h-full">{props.children[1]}</aside>
+      <aside className="flex w-full md:w-3/5 h-full">{props.children[1]}</aside>
     </div>
   );
 };
