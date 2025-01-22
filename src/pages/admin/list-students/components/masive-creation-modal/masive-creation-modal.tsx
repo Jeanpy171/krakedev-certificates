ff4aa1dde5167700f8c0b47c1d7af82dd8dc727c
Jeanpy171 <jeanpyfuentes67@gmail.com>
2025-01-21 22:04:13 -0500

    fix certiifcates

--- src/pages/admin/list-students/components/masive-creation-modal/masive-creation-modal.tsx
+++ src/pages/admin/list-students/components/masive-creation-modal/masive-creation-modal.tsx
@@ -86,10 +86,13 @@ const parseExcelFile = (
           }
 
           const diplomaParts = diploma.split(" ");
-          const range = diplomaParts.slice(1).join(" ") || "";
+          const range =
+            diplomaParts[0].toLowerCase() === "diploma"
+              ? diplomaParts.slice(1).join(" ")
+              : diplomaParts.join(" ");
 
-          const certificateData = certificate?.templates.find((template) =>
-            template.range.includes(range)
+          const certificateData = certificate?.templates.find(
+            (template) => template.range.toLowerCase() === range.toLowerCase()
           );
 
           if (!certificateData) {
